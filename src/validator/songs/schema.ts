7bbe6b874e6a102a5122e046738e0f665cad4f20
agis <agista@dicoding.com>
2021-08-05 15:08:33 +0700

    use strict when validate number type

--- src/validator/songs/schema.ts
+++ src/validator/songs/schema.ts
@@ -2,10 +2,10 @@ import Joi from "joi";
 
 const SongPayloadSchema = Joi.object({
     title: Joi.string().required(),
-    year: Joi.number().required(),
+    year: Joi.number().required().strict(true),
     performer: Joi.string().required(),
     genre: Joi.string(),
-    duration: Joi.number()
+    duration: Joi.number().strict(true)
 })
 
 export default SongPayloadSchema
