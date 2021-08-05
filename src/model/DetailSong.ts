5c69e440e43201920310568a4800dbe21764ba5e
agis <agista@dicoding.com>
2021-08-05 11:09:46 +0700

    create song model

---
+++ src/model/DetailSong.ts
@@ -0,0 +1,12 @@
+interface DetailSong{
+    id: string,
+    "title": string,
+    "year": number,
+    "performer": string,
+    "genre": string,
+    "duration": number,
+    "insertedAt": string,
+    "updatedAt": string
+}
+
+export default DetailSong
---
+++ src/model/request/SongRequest.ts
@@ -0,0 +1,9 @@
+interface SongRequest{
+    "title": string,
+    "year": number,
+    "performer": string,
+    "genre": string,
+    "duration": number
+}
+
+export default SongRequest
