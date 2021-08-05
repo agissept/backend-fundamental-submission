db4c15c03994b0c66b2896ba39a618b60cab17bf
agis <agista@dicoding.com>
2021-08-05 11:09:52 +0700

    create song service

---
+++ src/services/SongsService.ts
@@ -0,0 +1,28 @@
+import {nanoid} from "nanoid";
+import SongRequest from "../model/request/SongRequest"
+import DetailSong from "../model/DetailSong";
+
+class SongsService {
+    private songs: Array<DetailSong> = []
+
+    addSong(payload: SongRequest) {
+        const songId = nanoid(16)
+
+        const song: DetailSong = {
+            ...payload,
+            id: songId,
+            insertedAt: new Date().toDateString(),
+            updatedAt: new Date().toDateString()
+        }
+
+        this.songs.push(song)
+
+        return songId
+    }
+
+    getAllSongs(){
+        return this.songs
+    }
+}
+
+export default SongsService
