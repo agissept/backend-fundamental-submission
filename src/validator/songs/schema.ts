cefbe385e7156aa235c8d0ab7e4aa89a640c5e91
agis <agista@dicoding.com>
2021-08-06 10:28:44 +0700

    add year limit validation

--- src/validator/songs/schema.ts
+++ src/validator/songs/schema.ts
@@ -2,7 +2,7 @@ import Joi from 'joi'
 
 const SongPayloadSchema = Joi.object({
   title: Joi.string().required(),
-  year: Joi.number().required().strict(true),
+  year: Joi.number().min(1900).max(2021).required().strict(true),
   performer: Joi.string().required(),
   genre: Joi.string(),
   duration: Joi.number().strict(true)
