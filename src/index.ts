ea7245525fb4d5a8b3498622b76cfad4abe0212d
Agis <agis.septiyanto@gmail.com>
2021-08-14 14:43:45 +0700

    register authentication plugin and users plugin

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import Hapi, { Request, ResponseToolkit } from '@hapi/hapi'
 import songs from './api/songs'
+import users from './api/users'
+import authentications from './api/authentications'
 import SongsService from './services/SongsService'
 import SongsValidator from './validator/songs'
 import dotenv from 'dotenv'
 import ClientError from './exception/ClientError'
 import ResponseSuccess from './model/ResponseSuccess'
+import UsersService from './services/UsersService'
+import UsersValidator from './validator/users'
+import TokenManager from './tokenize/TokenManager'
+import AuthenticationsValidator from './validator/authentication'
+import AuthenticationsService from './services/AuthenticationsService'
 
 const init = async () => {
   dotenv.config()
@@ -46,13 +53,29 @@ const init = async () => {
     }).code(statusCode)
   })
 
-  await server.register({
+  await server.register([{
     plugin: songs,
     options: {
       service: new SongsService(),
       validator: new SongsValidator()
     }
-  })
+  },
+  {
+    plugin: users,
+    options: {
+      service: new UsersService(),
+      validator: new UsersValidator()
+    }
+  },
+  {
+    plugin: authentications,
+    options: {
+      authenticationsService: new AuthenticationsService(),
+      usersService: new UsersService(),
+      tokenManager: new TokenManager(),
+      validator: new AuthenticationsValidator()
+    }
+  }])
 
   await server.start()
   console.log(`Server running on port ${server.info.uri}`)
