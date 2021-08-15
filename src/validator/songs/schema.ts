62a0263cce8761e1830f0979dd5f22ab58eb0565
Agis <agis.septiyanto@gmail.com>
2021-08-15 20:57:46 +0700

    use dynamic year limit

--- src/validator/songs/schema.ts
+++ src/validator/songs/schema.ts
@@ -2,7 +2,7 @@ import Joi from 'joi'
 
 const SongPayloadSchema = Joi.object({
   title: Joi.string().required().strict(true),
-  year: Joi.number().min(1900).max(2021).required().strict(true),
+  year: Joi.number().min(1900).max(new Date().getFullYear()).required().strict(true),
   performer: Joi.string().required().strict(true),
   genre: Joi.string().strict(true),
   duration: Joi.number().strict(true)
